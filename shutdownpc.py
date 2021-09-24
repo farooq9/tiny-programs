b1b6749f9f4c262ee014205a1f892d1af23c6ae8
Abdul Farooq <88651754+farooq9@users.noreply.github.com>
2021-09-24 16:32:02 +0530

    Update shutdownpc.py
    This is a python program to shutdown pc using a pin, when the pin is incorrect it displays a popup which says that "your pin is incorrect please keep practicing python" , the correct pin for this program is 1234 and if the pin is incorrect then the popup will display and say "your pc is not gonna shutdown".

--- shutdownpc.py
+++ shutdownpc.py
@@ -1,15 +1,16 @@
 # program to shutdown pc
+#This program asks the user to enter pin to shutdown pc
 import pyautogui as po
 import os
 
 options = po.confirm('Do you want to shutdown pc?', buttons= ['Yes', 'No'])
-pin = '1234'
+pin = '1234' #This is the correct pin
 
 if options == 'Yes':
     a = po.password('Enter pin to Shutdown PC')
     if a == pin:
         print('pc is shutting down in 5 seconds')
-        # os.system('shutdown /s /t 5')
+        os.system('shutdown /s /t 5')
     else:
 
         po.alert('Your pin is Incorrect\nPlease keep practicing Python')
