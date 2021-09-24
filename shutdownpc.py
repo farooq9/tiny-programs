dc8c322d92e9067dd6edc19d363b5c0242d725a1
Abdul Farooq <88651754+farooq9@users.noreply.github.com>
2021-09-24 16:27:23 +0530

    Add files via upload

---
+++ shutdownpc.py
@@ -0,0 +1,20 @@
+# program to shutdown pc
+import pyautogui as po
+import os
+
+options = po.confirm('Do you want to shutdown pc?', buttons= ['Yes', 'No'])
+pin = '1234'
+
+if options == 'Yes':
+    a = po.password('Enter pin to Shutdown PC')
+    if a == pin:
+        print('pc is shutting down in 5 seconds')
+        # os.system('shutdown /s /t 5')
+    else:
+
+        po.alert('Your pin is Incorrect\nPlease keep practicing Python')
+else:
+    po.alert('Your pc is not gonna shutdown.')
+    exit()
+
+print(f'Your pin:{a}\nSystem pin {pin} ')
